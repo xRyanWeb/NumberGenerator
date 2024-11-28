2fdfa7dd603762060a75559eba7a542ddac61fde
xRyanWeb <ryangregoire286@gmail.com>
2024-11-28 09:48:55 -0500

    Added More Functioms

--- javascript/Functions.ts
+++ javascript/Functions.ts
@@ -1,3 +1,14 @@
-export function getFunction(a: HTMLElement) {}
+import { number_1, number_2 } from "../misc/App";
+import { Add } from "./Added";
+
+const input = document.getElementById("input") as HTMLInputElement;
+
+const value: any = input.value;
+
+export function getFunction() {
+  if (value == Add(number_1, number_2)) {
+    alert("Correct");
+  }
+}
 
 const button1 = document.getElementById("addButton") as HTMLButtonElement;
--- misc/App.ts
+++ misc/App.ts
@@ -1,2 +1,4 @@
-const number_1 = Math.floor(Math.random() * 10 + 1);
-const number_2 = Math.floor(Math.random() * 5 + 1);
+export const number_1: any = Math.floor(Math.random() * 10 + 1);
+export const number_2: any = Math.floor(Math.random() * 5 + 1);
+
+
